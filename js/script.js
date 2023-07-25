e633b6a7d53c10fec9dad9b22cd2f3d08b300030
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-07-25 12:12:34 -0600

    more game logic
    more game logic plus board

--- js/script.js
+++ js/script.js
@@ -35,3 +35,99 @@ function checkWin(row, col){
         [-1,0], [1,0], [0,-1], [0,1], [-1,-1], [1,1], [-1,1], [1,-1]
     ];
 }
+
+function countMoves(rowDirection, colDirection) {
+    let count = 1;
+    for (let i = 1; i < winCount; i++) {
+      const r = row + rowDirection * i;
+      const c = col + colDirection * i;
+      if (r < 0 || r >= board || c < 0 || c >= board || board[r][c] !== board[row][col]) {
+        break;
+      }
+      count++;
+    }
+    for (let i = 1; i < winCount; i++) {
+      const r = row - rowDirection * i;
+      const c = col - colDirection * i;
+      if (r < 0 || r >= board || c < 0 || c >= board || board[r][c] !== board[row][col]) {
+        break;
+      }
+      count++;
+    }
+    return count;
+  }
+
+  for (const [rowDirection, colDirection] of directions) {
+    const count = countMoves(rowDirection, colDirection);
+    if (count >= winCount) {
+      return winCount;
+    } else if (count >= countFour) {
+      return countFour;
+    } else if (count >= countThree) {
+      return countThree;
+    }
+  }
+  return 0;
+
+
+function makeMove(row, col) {
+    if (gameOver || board[row][col] !== boardEmpty) {
+      return;
+    }
+
+    board[row][col] = currentPlayer;
+    drawBoard();
+
+    const winCount = checkWin(row, col);
+
+    if (winCount === winCount) {
+      alert(`Player ${currentPlayer} wins with 5 in a row!`);
+      gameOver = true;
+    } else if (winCount === countFour) {
+      alert(`Player ${currentPlayer} has 4 in a row!`);
+    } else if (winCount === countThree) {
+      alert(`Player ${currentPlayer} has 3 in a row!`);
+    }
+
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  }
+
+  function makeGameBoard() {
+    const container = document.getElementById('board');
+    container.innerHTML = '';
+    for (let i = 0; i < gameBoard; i++) {
+      for (let q = 0; q < gameBoard; q++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.row = i;
+        cell.dataset.col = q;
+        if (board[i][q] === playerOne) {
+          cell.style.backgroundColor = 'black';
+        } else if (board[i][q] === playerTwo) {
+          cell.style.backgroundColor = 'white';
+        }
+        container.appendChild(cell);
+      }
+    }
+  }
+
+  function handleCellClick(event) {
+    const row = parseInt(event.target.dataset.row);
+    const col = parseInt(event.target.dataset.col);
+    makeMove(row, col);
+  }
+
+  function setupEventListeners() {
+    const cells = document.getElementsByClassName('cell');
+    for (const cell of cells) {
+      cell.addEventListener('click', handleCellClick);
+    }
+  }
+
+  function startGame() {
+    getBoard();
+    makeGameBoard();
+    setupEventListeners();
+  }
+
+  startGame();
