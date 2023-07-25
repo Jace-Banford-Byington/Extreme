89f1ad100613646ecb7ea5ecd6bba8a8c02d4c7c
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-07-25 12:42:36 -0600

    debugging
    debugging game

--- js/script.js
+++ js/script.js
@@ -15,11 +15,13 @@ playerTwoCaps = 0;
 
 gameOver = false;
 
+startGame();
+
 function getBoard(){
     for(let i = 0; i < gameBoard; i++){
         board[i] = [];
         for(let q = 0; q < gameBoard; q++){
-            baord[i][q] = boardEmpty;
+            board[i][q] = boardEmpty;
         }
     }
 }
@@ -30,11 +32,57 @@ function getNames(){
 
 }
 
-function checkWin(row, col){
-    const directions = [
-        [-1,0], [1,0], [0,-1], [0,1], [-1,-1], [1,1], [-1,1], [1,-1]
-    ];
-}
+function checkWin(row, col) {
+    function countMoves(rowDirection, colDirection) {
+      let count = 1;
+      for (let i = 1; i < winCount; i++) {
+        const r = row + rowDirection * i;
+        const c = col + colDirection * i;
+        if (r < 0 || r >= gameBoard || c < 0 || c >= gameBoard || board[r][c] !== board[row][col]) {
+          break;
+        }
+        count++;
+      }
+      for (let i = 1; i < winCount; i++) {
+        const r = row - rowDirection * i;
+        const c = col - colDirection * i;
+        if (r < 0 || r >= gameBoard || c < 0 || c >= gameBoard || board[r][c] !== board[row][col]) {
+          break;
+        }
+        count++;
+      }
+      return count;
+    }
+
+    for (const [rowDirection, colDirection] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
+      const count = countMoves(rowDirection, colDirection);
+      if (count >= winCount) {
+        return winCount;
+      } else if (count >= countFour) {
+        return countFour;
+      } else if (count >= countThree) {
+        return countThree;
+      }
+    }
+    return 0;
+  }
+
+function captureOpponentPieces(row, col, rowDirection, colDirection) {
+    const opponent = currentPlayer === playerOne ? playerTwo : playerOne;
+    const r = row + rowDirection;
+    const c = col + colDirection;
+    if (r >= 0 && r < gameBoard && c >= 0 && c < gameBoard && board[r][c] === opponent) {
+      board[r][c] = boardEmpty;
+      if (opponent === playerOne) {
+        player1Captured++;
+        document.getElementById('playerOneCapturedText').textContent = `${playerOneName} Captured: ${player1Captured}`;
+      } else {
+        player2Captured++;
+        document.getElementById('playerTwoCapturedText').textContent = `${playerTwoName} Captured: ${player2Captured}`;
+      }
+      captureOpponentPieces(r, c, rowDirection, colDirection);
+    }
+  }
 
 function countMoves(rowDirection, colDirection) {
     let count = 1;
@@ -57,36 +105,32 @@ function countMoves(rowDirection, colDirection) {
     return count;
   }
 
-  for (const [rowDirection, colDirection] of directions) {
-    const count = countMoves(rowDirection, colDirection);
-    if (count >= winCount) {
-      return winCount;
-    } else if (count >= countFour) {
-      return countFour;
-    } else if (count >= countThree) {
-      return countThree;
-    }
-  }
-  return 0;
 
-
-function makeMove(row, col) {
+  function makeMove(row, col) {
     if (gameOver || board[row][col] !== boardEmpty) {
       return;
     }
 
     board[row][col] = currentPlayer;
-    drawBoard();
+    makeGameBoard();
 
     const winCount = checkWin(row, col);
 
     if (winCount === winCount) {
-      alert(`Player ${currentPlayer} wins with 5 in a row!`);
+      alert(`${currentPlayer === playerOne ? playerOneName : playerTwoName} wins with 5 in a row!`);
       gameOver = true;
     } else if (winCount === countFour) {
-      alert(`Player ${currentPlayer} has 4 in a row!`);
-    } else if (winCount === countThree) {
-      alert(`Player ${currentPlayer} has 3 in a row!`);
+      alert(`${currentPlayer === playerOne ? playerOneName : playerTwoName} has 4 in a row!`);
+    } else if (winCount >= countThree) {
+      let message = `${currentPlayer === playerOne ? playerOneName : playerTwoName} has ${winCount} in a row!`;
+      if (winCount === countThree) {
+        message += "\nCapturing opponent's pieces!";
+      }
+      alert(message);
+      const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+      for (const [rowDirection, colDirection] of directions) {
+        captureOpponentPieces(row, col, rowDirection, colDirection);
+      }
     }
 
     currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
@@ -104,7 +148,7 @@ function makeMove(row, col) {
         if (board[i][q] === playerOne) {
           cell.style.backgroundColor = 'black';
         } else if (board[i][q] === playerTwo) {
-          cell.style.backgroundColor = 'white';
+          cell.style.backgroundColor = 'red';
         }
         container.appendChild(cell);
       }
@@ -130,4 +174,3 @@ function makeMove(row, col) {
     setupEventListeners();
   }
 
-  startGame();
